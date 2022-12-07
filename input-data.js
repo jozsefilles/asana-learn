f205c1c7599fcc80974c7b6613f508de74663f33
Jozsef Illes <illes.jozsef@meltwater.com>
2022-12-07 22:41:24 +0100

    Separate input data

---
+++ input-data.js
@@ -0,0 +1,47 @@
+export const ASANAS_CSV = `
+Padahastasana;Vorwärtsbeuge im Stehen (Geste oder Hände zu den Füssen);Muladhara;tellurische
+Tadasana;Baumposition;-;kosmische
+Sahaja Agnisara Dhauti;Natürliche Reinigung mittels der Feueressenz;Manipura;biomagnetische
+Trikonasana;Dreieckpose;"Manipura
+Chandra (linke Arm)
+Surya (rechte Arm)";kosmische + tellurische
+Uddiyana Bandha;"Kräftiges Einziehen des Bauches
+Pfad des Aufwärtsfliegens";Manipura;biomagnetische
+"Garudasana
+(Vrkasana)";Adlerpose;"Ajna
+";"biomagnetische
+kosmische"
+Prasarita Padottanasana;Vorwärtsbeuge mit gespreitzten Füssen;"Muladhara
+Sahasrara";tellurische
+Shalabasana;Grashüpfer Pose;Svadhistana;kosmische
+Dhanurasana;Position den Bogens;Manipura;"biomagnetische
+tellurische"
+Pavana Muktasana;"Windlösende Stellung
+Windbefreiende Pose";Manipura;kosmische?
+Bhujangasana;Kobrapose;Anahata;kosmische + tellurische
+Pascimottanasana;Vorwärtsbeuge im Sitzen/Stellung der hinteren Dehung/Stellung des Aufsteigens nach Westen;Muladhara;tellurische
+Cakrasana;Radpose;Dominantes;tellurische
+Gomukhasana;Kuhmaulposition;"Anahata - (linke Arm)
+Anahata + (rechte Arm)";kosmische
+Ardha Matsyendrasana;Halbe Fischgottpose;Visuddha;kosmische
+Svastikasana;Glückposition;"Manipura
+Ajna";kosmische
+Vajrasana;"Diamantensitz
+Position des Donnerkeil";"Ajna
+Anahata";kosmische
+Sarvangasana;"Kerze
+Schulterstand";Visuddha;kosmische
+Halasana;Pflugpose;Ajna;"tellurische
+kosmische"
+Sirsasana;Kopfstand;Sahasrara;"kosmische
+tellurische"
+Janusirsasana;Kopf zum Knie Pose;Muladhara;tellurische
+Trikonasana - die Vorwärtsversion;Dreieckpose - die Vorwärtsversion;"Muladhara
+Manipura";"kosmische
+tellurische"
+Marjariasana;Katzen Dehnungs Pose;Manipura;biomagnetische
+Shashasana;Hasenpose;"Muladhara
+Sahasrara";kosmische
+Agnisara Dhauti;Reinigung mit der subtilen Essenz des Feuers;Anahata;biomagnetische
+Nauli Kriya;Wellenbewegungsreinigung;Manipura;biomagnetische
+`;
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import {Asana} from "./asana.js";
+import {ASANAS_CSV} from "./input-data";
 
 function parseInputCsv(inputCsv) {
     const parsed = [];
@@ -24,55 +25,7 @@ function parseInputCsv(inputCsv) {
     return parsed;
 }
 
-const asanasCsv = `
-Padahastasana;Vorwärtsbeuge im Stehen (Geste oder Hände zu den Füssen);Muladhara;tellurische
-Tadasana;Baumposition;-;kosmische
-Sahaja Agnisara Dhauti;Natürliche Reinigung mittels der Feueressenz;Manipura;biomagnetische
-Trikonasana;Dreieckpose;"Manipura
-Chandra (linke Arm)
-Surya (rechte Arm)";kosmische + tellurische
-Uddiyana Bandha;"Kräftiges Einziehen des Bauches
-Pfad des Aufwärtsfliegens";Manipura;biomagnetische
-"Garudasana
-(Vrkasana)";Adlerpose;"Ajna
-";"biomagnetische
-kosmische"
-Prasarita Padottanasana;Vorwärtsbeuge mit gespreitzten Füssen;"Muladhara
-Sahasrara";tellurische
-Shalabasana;Grashüpfer Pose;Svadhistana;kosmische
-Dhanurasana;Position den Bogens;Manipura;"biomagnetische
-tellurische"
-Pavana Muktasana;"Windlösende Stellung
-Windbefreiende Pose";Manipura;kosmische?
-Bhujangasana;Kobrapose;Anahata;kosmische + tellurische
-Pascimottanasana;Vorwärtsbeuge im Sitzen/Stellung der hinteren Dehung/Stellung des Aufsteigens nach Westen;Muladhara;tellurische
-Cakrasana;Radpose;Dominantes;tellurische
-Gomukhasana;Kuhmaulposition;"Anahata - (linke Arm)
-Anahata + (rechte Arm)";kosmische
-Ardha Matsyendrasana;Halbe Fischgottpose;Visuddha;kosmische
-Svastikasana;Glückposition;"Manipura
-Ajna";kosmische
-Vajrasana;"Diamantensitz
-Position des Donnerkeil";"Ajna
-Anahata";kosmische
-Sarvangasana;"Kerze
-Schulterstand";Visuddha;kosmische
-Halasana;Pflugpose;Ajna;"tellurische
-kosmische"
-Sirsasana;Kopfstand;Sahasrara;"kosmische
-tellurische"
-Janusirsasana;Kopf zum Knie Pose;Muladhara;tellurische
-Trikonasana - die Vorwärtsversion;Dreieckpose - die Vorwärtsversion;"Muladhara
-Manipura";"kosmische
-tellurische"
-Marjariasana;Katzen Dehnungs Pose;Manipura;biomagnetische
-Shashasana;Hasenpose;"Muladhara
-Sahasrara";kosmische
-Agnisara Dhauti;Reinigung mit der subtilen Essenz des Feuers;Anahata;biomagnetische
-Nauli Kriya;Wellenbewegungsreinigung;Manipura;biomagnetische
-`;
-
-const asanas = parseInputCsv(asanasCsv);
+const asanas = parseInputCsv(ASANAS_CSV);
 
 window.showNextAsana = function () {
     let i = Math.floor(Math.random() * asanas.length);

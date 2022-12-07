abc3e47a6fb14e2803ec6791b2ee2451bd04c853
Jozsef Illes <illes.jozsef@meltwater.com>
2022-12-07 22:50:27 +0100

    Updated input

--- input-data.js
+++ input-data.js
@@ -17,7 +17,7 @@ Shalabasana;Grashüpfer Pose;Svadhistana;kosmische
 Dhanurasana;Position den Bogens;Manipura;"biomagnetische
 tellurische"
 Pavana Muktasana;"Windlösende Stellung
-Windbefreiende Pose";Manipura;kosmische?
+Windbefreiende Pose";Manipura;kosmische
 Bhujangasana;Kobrapose;Anahata;kosmische + tellurische
 Pascimottanasana;Vorwärtsbeuge im Sitzen/Stellung der hinteren Dehung/Stellung des Aufsteigens nach Westen;Muladhara;tellurische
 Cakrasana;Radpose;Dominantes;tellurische
@@ -35,6 +35,7 @@ Halasana;Pflugpose;Ajna;"tellurische
 kosmische"
 Sirsasana;Kopfstand;Sahasrara;"kosmische
 tellurische"
+;;;
 Janusirsasana;Kopf zum Knie Pose;Muladhara;tellurische
 Trikonasana - die Vorwärtsversion;Dreieckpose - die Vorwärtsversion;"Muladhara
 Manipura";"kosmische
@@ -44,4 +45,16 @@ Shashasana;Hasenpose;"Muladhara
 Sahasrara";kosmische
 Agnisara Dhauti;Reinigung mit der subtilen Essenz des Feuers;Anahata;biomagnetische
 Nauli Kriya;Wellenbewegungsreinigung;Manipura;biomagnetische
+Jivabalasana;Lebenskraftpose;"12 Sekundären Chakren
+an den Gelenken";"kosmische
+tellurische"
+Trikonasana - Eingedreht;Dreieckpose - Eingedreht;"Manipura
+Chandra (linke Arm)
+Surya (rechte Arm)
+Visudha (sekundär)";kosmische + tellurische
+Nabhiasana;Bauchnabel Pose;Manipura;kosmische
+Utkatasana;"Aufsteigende 
+geduckte Pose";"Muladhara
+Ajna";tellurische
+Sukhasana;Angenehme Pose;Svadhistana +/-;tellurische
 `;
